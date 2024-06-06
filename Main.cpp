307977b45f4bee8704683bff0b8924f6514d3cbf
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2024-06-06 02:15:10 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,15 +7,16 @@
 using namespace std;
 
 int main() {
-
     Cuenta cuenta;
     Comida* Carajillo = new Bebida("Carajillo", "CAfe con Licor 43", 3.99, "Carajillo", .20, 600, true);
     Comida* Cocacola = new Bebida("Cocacola", "Refresco Coca cola Norm", 1.99, "Carajillo", .30, 600, false);
     Comida* pastelChocolate = new Postre("Pastel chocolate", "Pastel", 5.99, 2, 50, 200, "Chocolate");
-    Comida* spaghetti = new Platillo("Spaghetti", "Spaghetti con salsa de tomate", 8.0);
+    Comida* spaghetti = new Platillo("Spaghetti", "Spaghetti con salsa de tomate", 8.0, 120, 1.89, true, false);
     cout << pastelChocolate ->toString() << endl;
     cout << Carajillo ->toString() << endl;
     cout << Cocacola ->toString() << endl;
+    cout << spaghetti ->toString() << endl;
+
 
     cuenta.agregarComida(Carajillo);
     cuenta.agregarComida(Cocacola);
